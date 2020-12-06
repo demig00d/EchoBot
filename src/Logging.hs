86d80ed9c2f577bc5f0f467ba2428d288a6d9d07
demig00d <28487425+demig00d@users.noreply.github.com>
2020-12-06 15:44:10 +0300

    Implement basic log functions

--- src/Logging.hs
+++ src/Logging.hs
@@ -1,16 +1,30 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Logging where
 
-import           Utils (deriveJSON)
+import           Prelude hiding (log)
+
+import           Utils   (deriveJSON)
 
 
 data Priority
   = Debug
   | Info
-  | Warn
+  | Warning
   | Error
-  | Fatal
   deriving (Show, Eq, Ord)
 
-
 $(deriveJSON ''Priority)
+
+
+log :: Priority -> Priority -> String -> IO ()
+log msgLvl appLvl msg =
+  if msgLvl >= appLvl
+     then putStrLn $ "[" <> show msgLvl <> "] " <> msg
+     else return ()
+
+logDebug, logInfo, logWarning :: Priority -> String -> IO ()
+logDebug   = log Debug
+logInfo    = log Info
+logWarning = log Warning
+
+logError = putStrLn . ("[Error] "<>)
