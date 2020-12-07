e4e4611a87b4d6056e84f0735f9821a285b49653
demig00d <28487425+demig00d@users.noreply.github.com>
2020-12-07 12:10:06 +0300

    Add more logging

--- src/Bot.hs
+++ src/Bot.hs
@@ -12,6 +12,7 @@ import           Bot.Types
 import           Bot.VKontakte          as VKontakte
 import           Configuration
 import           Logging
+import           Utils                  (prettyShow)
 
 
 startBot :: FilePath -> IO ()
@@ -24,21 +25,31 @@ startBot path = do
 
       logInfo cLogLevel ("Configuration file has been read and decoded." :: String)
       >> logInfo cLogLevel ("Telegram platform was selected." :: String)
-      >> Telegram.getModel cfg  >>= either logError (runReaderT mainLoop)
+      >> Telegram.getModel cfg
+      >>= either logError
+           (\model -> logInfo cLogLevel "Model extracted from Config"
+                   >> logDebug cLogLevel (prettyShow model)
+                   >> runReaderT mainLoop model)
 
     Right cfg@Config{cPlatformName="vkontakte",..} ->
       logInfo cLogLevel ("Configuration file has been read and decoded." :: String)
       >> logInfo cLogLevel ("VKontakte platform was selected." :: String)
-      >> VKontakte.getModel cfg >>= either logError (runReaderT mainLoop)
-
+      >> VKontakte.getModel cfg
+      >>= either logError
+           (\model -> logInfo cLogLevel "Model extracted from Config"
+                   >> logDebug cLogLevel (prettyShow model)
+                   >> runReaderT mainLoop model)
 
 mainLoop :: (Bot env, MonadReader (Model env) m, MonadIO m) => m ()
 mainLoop = do
   model@Model{..} <- ask
+
   u <- liftIO $ getUpdates mPlatformEnv
+
   case u of
     Left msg      -> liftIO $ logError msg
     Right updates -> do
+      liftIO $ logInfo mLogLevel "Bot got update."
+
       model' <- liftIO $ handleUpdates model updates
       runReaderT mainLoop model'
-
