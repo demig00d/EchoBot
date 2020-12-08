48fc83be5bf6d4affa5fadff06c02d65e013d3f0
demig00d <28487425+demig00d@users.noreply.github.com>
2020-12-08 04:49:21 +0300

    Implement getMe method

--- src/Telegram/API.hs
+++ src/Telegram/API.hs
@@ -1,6 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Telegram.API where
 
+import           Data.Aeson                 (eitherDecodeStrict)
+import qualified Data.ByteString.Lazy.Char8 as L8
 
-getMe :: String -> IO (Either String ())
-getMe _ = pure $ Right ()
+import           Requests
+import           Telegram.Types
+
+
+urlAPI = "https://api.telegram.org/bot"
+
+getMe :: String -> IO (Either L8.ByteString L8.ByteString)
+getMe token = do
+  resp <- sendGet $ urlAPI <> token <> "/getMe"
+  pure $ resp >>= verify
+    where
+      verify bs = case L8.take 10 . L8.drop 6 $ bs of
+           "\"ok\": true" -> Right bs
+           _              -> Left bs
+
 
