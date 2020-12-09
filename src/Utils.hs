cfb8e9a51502832d75ef134ad5013858be72bdb9
demig00d <28487425+demig00d@users.noreply.github.com>
2020-12-09 21:17:01 +0300

    Implement some new utility-functions, add comments along with inlining and specializations

--- src/Utils.hs
+++ src/Utils.hs
@@ -3,16 +3,25 @@ module Utils
   , deriveManyJSON
   --
   , prettyShow
+  , prettyShowMap
+  , gshow
   --
   , eitherDecode
+  --
+  , lookupInsert
   ) where
 
-import           Data.Aeson    (camelTo2)
-import qualified Data.Aeson    as Aeson (eitherDecode)
-import qualified Data.Aeson.TH as TH (defaultOptions, deriveJSON,
-                                      fieldLabelModifier, omitNothingFields)
-import           Data.Char     (isUpper, toLower)
-import           Data.String   (fromString)
+import           Data.Aeson                 (FromJSON, camelTo2)
+import qualified Data.Aeson                 as Aeson (eitherDecode)
+import qualified Data.Aeson.TH              as TH (defaultOptions, deriveJSON,
+                                                   fieldLabelModifier,
+                                                   omitNothingFields)
+import           Data.ByteString.Char8      as S8 (ByteString)
+import           Data.ByteString.Lazy.Char8 as L8 (ByteString)
+import           Data.Char                  (isUpper, toLower)
+import           Data.Map.Strict            as Map (Map, insert, lookup, toList)
+import           Data.String                (IsString, fromString)
+import           Data.Text                  (Text)
 
 
 deriveJSON = TH.deriveJSON TH.defaultOptions
@@ -26,11 +35,22 @@ deriveJSON = TH.deriveJSON TH.defaultOptions
 deriveManyJSON names = concat <$> mapM deriveJSON names
 
 
+-- | Generalized version of show. Unlike show this function
+-- is polymorphic in its result type.
+gshow :: (Show a, IsString b) => a -> b
+gshow x = fromString $ show x
+{-# INLINE gshow #-}
+{-# SPECIALIZE gshow :: Show a => a -> Text  #-}
+{-# SPECIALIZE gshow :: Show a => a -> S8.ByteString  #-}
+{-# SPECIALIZE gshow :: Show a => a -> L8.ByteString  #-}
+{-# SPECIALIZE gshow :: Show a => a -> String  #-}
+
 -- | Pretty show for data types with fields that have a Show
 -- instance, breaks with more then one parenthesis at the end.
 prettyShow :: Show a => a -> String
 prettyShow str = unwords $ (pretty "" False) . words $ show str
 
+-- | Helper for prettyShow.
 pretty :: String -> Bool -> [String] -> [String]
 pretty indent addComma (s:ss)
   | lastP        = (init s <> "\n" <> indent <> " }")             : []
@@ -50,4 +70,26 @@ pretty indent addComma (s:ss)
     lastC        = last s == ','
 
 
+-- | Helper function for Map pretty-printing.
+-- Used only in Bot module to prettify particular
+-- log message like this:
+-- [Debug] Map of user_id and repeat_number:
+--                (100000000,       4      )
+--                (100000001,       2      )
+prettyShowMap m =
+  mconcat $ (\(k, v) -> "               " <> "(" <> show k <> ","
+                     <> "       " <> show v
+                     <> "      " <> ")\n") <$> (Map.toList m)
+
+
+-- | Generalized version of eitherDecode from Data.Aeson.
+eitherDecode :: (IsString a, FromJSON b) => L8.ByteString -> Either a b
 eitherDecode bs = either (Left . fromString) Right (Aeson.eitherDecode bs)
+{-# INLINE eitherDecode #-}
+{-# SPECIALIZE eitherDecode :: FromJSON b => L8.ByteString -> Either L8.ByteString b #-}
+
+
+-- | Lookup value in Map or insert defailt value if it does not exist yet.
+lookupInsert :: Ord k => k -> a -> Map k a -> (a, Map k a)
+lookupInsert key defaultValue dict =
+  maybe (defaultValue, Map.insert key defaultValue dict) (\v -> (v, dict)) (Map.lookup key dict)
