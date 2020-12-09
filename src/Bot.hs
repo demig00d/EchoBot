b3eba55f946a39d0648258de01e9d1c67e8c9b12
demig00d <28487425+demig00d@users.noreply.github.com>
2020-12-09 21:33:56 +0300

    Add more logging

--- src/Bot.hs
+++ src/Bot.hs
@@ -6,13 +6,14 @@ module Bot (startBot) where
 import           Control.Monad.IO.Class
 import           Control.Monad.Reader
 import qualified Data.ByteString.Char8  as S8
+import           Data.Map.Strict
 
 import           Bot.Telegram           as Telegram
 import           Bot.Types
 import           Bot.VKontakte          as VKontakte
 import           Configuration
 import           Logging
-import           Utils                  (prettyShow)
+import           Utils                  (prettyShow, prettyShowMap)
 
 
 startBot :: FilePath -> IO ()
@@ -43,17 +44,18 @@ startBot path = do
                    >> logDebug cLogLevel (prettyShow model)
                    >> runReaderT mainLoop model)
 
-
 mainLoop :: (Bot env, MonadReader (Model env) m, MonadIO m) => m ()
 mainLoop = do
   model@Model{..} <- ask
+  liftIO $ logDebug mLogLevel ("Map of user_id and repeat_number:\n" <> prettyShowMap mUsersSettings)
 
-  u <- liftIO $ getUpdates mPlatformEnv
+  liftIO $ logInfo mLogLevel "Receive incoming updates."
+  u <- liftIO $ getUpdates model
 
   case u of
-    Left msg      -> liftIO $ logError msg
+    Left msg      -> liftIO $ logWarning mLogLevel msg
     Right updates -> do
-      liftIO $ logInfo mLogLevel "Bot got update."
+      liftIO $ logInfo mLogLevel "Bot got updates."
 
       model' <- liftIO $ handleUpdates model updates
       runReaderT mainLoop model'
