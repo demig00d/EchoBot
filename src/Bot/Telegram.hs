cc4d278a9a5b1472856f9df42348b49b968a92fc
demig00d <28487425+demig00d@users.noreply.github.com>
2020-12-09 21:33:24 +0300

    Implement Bot instance

--- src/Bot/Telegram.hs
+++ src/Bot/Telegram.hs
@@ -1,26 +1,129 @@
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TypeFamilies      #-}
-module Bot.Telegram where
+module Bot.Telegram (getModel) where
 
+import           Control.Monad
 import qualified Data.ByteString.Lazy.Char8 as L8
+import           Data.Map.Strict            (fromList, insert)
+import           Data.Text                  (Text)
+import           Text.Read                  (readMaybe)
 
-import           Bot.Types
+import           Bot.Types                  as Bot
 import           Logging
 import           Telegram.API
+import qualified Telegram.API               as Telegram (copyMessage,
+                                                         getUpdates,
+                                                         sendMessage)
 import           Telegram.Types
+import           Utils                      (eitherDecode, gshow, lookupInsert)
 
 
-instance Bot TelegramEnv where
+instance Bot.Bot TelegramEnv where
   type BotUpdate TelegramEnv = Update
-  getUpdates   = undefined
-  handleUpdate = undefined
 
-data TelegramEnv =
-  TelegramEnv
-    { getToken :: String
-    , offset   :: Int
-    } deriving Show
+  getUpdates model = do
+    logInfo' logLevel "Send 'getUpdates' method and wait for response."
+
+    eRespBS <- Telegram.getUpdates token offset
+    logInfo' logLevel "Response recieved."
+    logDebug logLevel (("\n"<>) <$> eRespBS)
+    pure $ do
+      respBS <- eRespBS
+      resp   <- eitherDecode respBS
+      maybe (Left "Failed on getting Update.") Right (rResult resp)
+      where
+        TelegramEnv{..} = mPlatformEnv model
+        logLevel = mLogLevel model
+
+
+  handleUpdate model = \case
+    updCbq@Update{rCallbackQuery = Just (CallbackQuery cdata _cFrom), ..} ->
+      case readMaybe cdata :: Maybe Int of
+        Just n -> handleAction model updCbq (SetRepeats n)
+        _      -> logWarning (mLogLevel model) ("Can't parse callbackquery: " <> cdata)
+               >> pure model
+
+    updMsg@Update{rMessage = Just message} ->
+      case mText message of
+        Just "/start"  -> handleAction model updMsg ShowStart
+        Just "/help"   -> handleAction model updMsg ShowHelp
+        Just "/repeat" -> handleAction model updMsg ShowRepeat
+        _              -> handleAction model updMsg Echo
+
+    _  -> logWarning' (mLogLevel model) "Can't handle Update" >> pure model
+
+
+data Action
+  = ShowStart
+  | ShowHelp
+  | ShowRepeat
+  | Echo
+  | SetRepeats Int
+
+
+handleAction model Update{..} action =
+  case rMessage of
+    Just message ->
+      let chatId     = cId $ mChat message
+          fromChatId = uId $ mFrom message
+          messageId  = mMessageId  message
+          firstName = uFirstName $ mFrom message
+
+          startMessage = "Hi, " <> firstName <> "! " <> bHelpMessage
+
+          (n, mUsersSettings') = lookupInsert fromChatId bNumberOfRepeats mUsersSettings
+
+          mPlatformEnv' = mPlatformEnv{offset = rUpdateId + 1}
+          model' = model{mUsersSettings=mUsersSettings', mPlatformEnv=mPlatformEnv'}
+
+          numKeyboard = mkKeyboard $ fmap (\x -> (show x, show x)) [1..5]
+
+      in case action of
+        ShowStart    -> sendMessage token chatId startMessage Nothing                 >> pure model'
+        ShowHelp     -> sendMessage token chatId bHelpMessage Nothing                 >> pure model'
+        ShowRepeat   -> sendMessage token chatId (repeatMessage n) (Just numKeyboard) >> pure model'
+        Echo         -> copyMessageN n token chatId fromChatId messageId              >> pure model'
+    _           ->
+      case rCallbackQuery of
+        Just (CallbackQuery cData cFrom) -> case action of
+          SetRepeats n ->
+            logInfo' mLogLevel ("Number of repeats for user: " <> gshow (uId cFrom) <> " changed to " <> gshow n <> ".")
+            >> pure model{ mUsersSettings=insert (uId cFrom) n mUsersSettings
+                         , mPlatformEnv=mPlatformEnv{offset=rUpdateId + 1}
+                         }
+        _         -> logWarning' mLogLevel "Unrecognized update." >> pure model
+  where
+    Model{..} = model
+    TelegramEnv{..} = mPlatformEnv
+    BotSettings{..} = mBotSettings
+
+    repeatMessage n = "Current number of repeats = " <> gshow n <> ".\n" <> bRepeatMessage
+
+    sendMessage t cid msg rm = do
+      logInfo' mLogLevel "Handle command."
+      logInfo' mLogLevel "Send request with 'sendMessage' method to reply to user's command."
+      eResponse <- Telegram.sendMessage t cid msg rm
+      case eResponse of
+        Left msg       -> logWarning mLogLevel msg
+        Right response -> logInfo' mLogLevel "Reply sended."
+                       >> logDebug mLogLevel ("\n" <> response)
+
+    copyMessageN n t cid fcid mid = do
+      logInfo' mLogLevel "Handle message."
+      logInfo' mLogLevel ("Number of repeats for user: " <> gshow fcid <> " is " <> gshow n <> ".")
+      let nTimes = \case
+               1   -> gshow 1 <> " time"
+               num -> gshow num <> " times"
+      logInfo' mLogLevel ("Send request with 'copyMessage' method " <> nTimes n <> " to echo user's message.")
+      replicateM_ n $ do
+            eResponse <- Telegram.copyMessage t cid fcid mid
+            case eResponse of
+              Left msg       -> logWarning mLogLevel msg
+              Right response -> logInfo' mLogLevel "Message has been echoed."
+                             >> logDebug mLogLevel ("\n" <> response)
+
 
 -- | Check request environment and try to get Model from Config.
 getModel :: Config -> IO (Either L8.ByteString (Model TelegramEnv))
@@ -39,6 +142,6 @@ getModel Config{..} = do
       model = Model
                 { mBotSettings   = cBotSettings
                 , mPlatformEnv   = TelegramEnv cToken 0
-                , mUsersSettings = []
+                , mUsersSettings = fromList []
                 , mLogLevel      = cLogLevel
                 }
